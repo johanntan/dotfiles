9381e73b98dd1127e4ca6961dcc01ea43e5d5a91
Johann <johann@jtan.me>
2025-10-03 14:27:42 +0800

    Update .config/fish/functions/fs.fish

--- home/dot_config/private_fish/functions/fs.fish
+++ home/dot_config/private_fish/functions/fs.fish
@@ -8,15 +8,15 @@ function fs --description 'Determine size of a file or total size of a directory
 
     if test (count $argv) -gt 0
         # respect `--` so filenames beginning with - are handled safely
-        command du $flags -- $argv
+        command du $flags -- $argv | string replace -r '/$' ''
     else
         # when no args, show sizes for entries in the current dir (including hidden, excluding . and ..)
         # fall back to current directory if there are no entries
         set -l entries (command ls -A 2>/dev/null)
         if test -n "$entries"
-            command du $flags $entries
+            command du $flags $entries | string replace -r '/$' ''
         else
-            command du $flags .
+            command du $flags . | string replace -r '/$' ''
         end
     end
 end
