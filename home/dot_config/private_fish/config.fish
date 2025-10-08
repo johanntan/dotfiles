0f1a501f2ce92776d55bedda15cd331c19045a0f
Johann <johann@jtan.me>
2025-10-08 20:44:10 +0800

    Update .config/fish/config.fish

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -3,4 +3,13 @@ if status is-interactive
 	set -g fish_autosuggestion_enabled 0
 	set -U fish_greeting
 end
-eval (/opt/homebrew/bin/brew shellenv)
+if not set -q HOMEBREW_PREFIX
+    if test -e /opt/homebrew/bin/brew
+        eval (/opt/homebrew/bin/brew shellenv)
+    else if test -e /home/.linuxbrew/bin/brew
+        eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    else
+        return 1
+    end
+end
+
