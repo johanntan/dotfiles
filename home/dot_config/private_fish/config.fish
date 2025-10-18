80309eda6fb83642991be0cb0c988ed0611eb9f7
Johann <johann@jtan.me>
2025-10-18 14:57:13 +0800

    Update .config/fish/config.fish

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
 	zoxide init fish | source
 	set -g fish_autosuggestion_enabled 0
 	set -U fish_greeting
+	source $HOME/.config/op/plugins.sh
 end
 set -gx DTK_PROGRAM swiftmac
