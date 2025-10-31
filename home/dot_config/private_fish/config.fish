8825a2929986550852d21d1525b11af5a3b0bc43
Johann <johann@jtan.me>
2025-10-31 16:06:18 +0800

    Update .config/fish/config.fish

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
 	zoxide init fish | source
-	atuin init fish | source
+	atuin init fish --disable-up-arrow | source
 	set -g fish_autosuggestion_enabled 0
 	set -U fish_greeting
 	source $HOME/.config/op/plugins.sh
