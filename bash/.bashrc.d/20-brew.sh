a42f0a6b5053c7bfc2812403eb13a9283e59256e
Johann <johann@jtan.me>
2026-03-26 11:05:32 +0800

    Added more aliases

--- bash/.bashrc.d/20-brew.sh
+++ bash/.bashrc.d/20-brew.sh
@@ -10,5 +10,10 @@ if [[ -n $location && -d $location ]]; then
 	export HOMEBREW_NO_ENV_HINTS=1
 	export HOMEBREW_NO_INSTALL_CLEANUP=1
 	export HOMEBREW_NO_AUTO_UPDATE=1
+	alias bu="brew update"
+	alias bug="brew upgrade"
+	alias bi="brew install"
+	alias bic="brew install --cask"
+	alias bcl="brew cleanup"
 	eval $($location/bin/brew shellenv)
 fi
--- bash/.bashrc.d/95-aliases.sh
+++ bash/.bashrc.d/95-aliases.sh
@@ -2,6 +2,8 @@ alias ll="ls -l"
 alias lsd="ls -lF | grep '^d'"
 alias reload="exec ${SHELL} -l"
 alias path='echo -e ${PATH//:/\\n}'
+alias fff=fanficfare
+alias ffu="fanficfare -u"
 if [[ `uname` == "Darwin" ]]; then
 	alias cpwd="pwd|tr -d '\n'|pbcopy"
 	alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
